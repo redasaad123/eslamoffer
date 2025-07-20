9ac9af4a2ca4f63882d9c2c3285bc45b6c74d24c
redasaad123 <redasaad010260@gmail.com>
2025-07-20 11:46:28 +0000

    c

--- ProjectApi/Program.cs
+++ ProjectApi/Program.cs
@@ -41,7 +41,7 @@ namespace ProjectApi
             //);
 
             builder.Services.AddDbContext<AppDBContext>(options =>
-                options.UseMySql(builder.Configuration.GetConnectionString("ProdcutionConnection"),
+                options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
                 new MySqlServerVersion(new Version(8, 0, 36))));
 
 
