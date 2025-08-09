5eebd549208120389a44fd2cfb0aeb6bcc54f412
Reda Saad <Reda.Saad@gmail.com>
2025-08-09 20:58:37 +0300

    commit

--- ProjectApi/Controllers/SitemapController.cs
+++ ProjectApi/Controllers/SitemapController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,70 +9,72 @@ using System.Xml.Linq;
 namespace YourNamespace.Controllers
 {
     [ApiController]
-    [Route("sitmap.xml")]
+    [Route("sitemap.xml")]
     public class SitemapController : ControllerBase
     {
+        private readonly AppDBContext _context;
+        public SitemapController(AppDBContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
-        public IActionResult GetSitemap()
+        public IActionResult Get()
         {
             string baseUrl = "https://api.eslamoffers.com";
 
-            var endpoints = new List<string>
-            {
-                //"/api/Account/GetUsers",
-                //"/api/Account/GetUser",
-                //"/api/Authenticate/RefreshToken",
-                "/api/Banner/GetAllBanners",
-                "/api/Category/GetAllCategories",
-                "/api/Coupons/GetAllCoupons",
-                "/api/Coupons/GetBestCoupons/Best",
-                "/api/Coupons/GetBestCoupons/BestDiscount",
-                "/api/Offers/GetAllOffers",
-                "/api/Offers/GetBestOffers/best",
-                "/api/Store/GetAllStores",
-                "/api/Store/GetBastStores/Bast",
-                "/api/SubscribeEmail/GetAllEmails"
-            };
+            // صفحات ثابتة
+            var staticPages = new List<string>
+        {
+            $"{baseUrl}/",
+            $"{baseUrl}/about",
+            $"{baseUrl}/contact"
+        };
 
-            var endpointMetadata = new Dictionary<string, (string changefreq, string priority)>
-            {
-                //{ "/api/Account/GetUsers", ("daily", "0.6") },
-                //{ "/api/Account/GetUser", ("daily", "0.6") },
-                { "/api/Authenticate/RefreshToken", ("monthly", "0.4") },
-                { "/api/Banner/GetAllBanners", ("weekly", "0.7") },
-                { "/api/Category/GetAllCategories", ("weekly", "0.8") },
-                { "/api/Coupons/GetAllCoupons", ("hourly", "0.9") },
-                { "/api/Coupons/GetBestCoupons/Best", ("hourly", "0.9") },
-                { "/api/Coupons/GetBestCoupons/BestDiscount", ("hourly", "0.9") },
-                { "/api/Offers/GetAllOffers", ("hourly", "1.0") },
-                { "/api/Offers/GetBestOffers/best", ("hourly", "1.0") },
-                { "/api/Store/GetAllStores", ("weekly", "0.8") },
-                { "/api/Store/GetBastStores/Bast", ("weekly", "0.8") },
-                { "/api/SubscribeEmail/GetAllEmails", ("monthly", "0.5") }
-            };
+            // روابط المتاجر
+            var storeUrls = _context.Stores
+                .Select(s => $"{baseUrl}/store/{s.Slug}" )
+                .ToList();
 
-            string today = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            // روابط التصنيفات
+            var categoryUrls = _context.Categories
+                .Select(c => $"{baseUrl}/category/{c.Slug}")
+                .ToList();
 
+            // روابط الكوبونات
+            var couponUrls = _context.Coupons
+                .Select(c => $"{baseUrl}/coupon/{c.Id}")
+                .ToList();
+
+            // روابط العروض
+            var offerUrls = _context.Offers
+                .Select(o => $"{baseUrl}/offer/{o.Id}")
+                .ToList();
+
+            // دمج كل الروابط
+            var allUrls = staticPages
+                .Concat(storeUrls)
+                .Concat(categoryUrls)
+                .Concat(couponUrls)
+                .Concat(offerUrls)
+                .Select(url => new { Url = url, LastModified = DateTime.UtcNow });
+
+            // إنشاء XML
             XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
-            var urlset = new XElement(ns + "urlset");
+            var sitemap = new XElement(ns + "urlset",
+                from page in allUrls
+                select new XElement(ns + "url",
+                    new XElement(ns + "loc", page.Url),
+                    new XElement(ns + "lastmod", page.LastModified.ToString("yyyy-MM-dd")),
+                    new XElement(ns + "changefreq", "daily"),
+                    new XElement(ns + "priority", "0.5")
+                    
 
-            foreach (var endpoint in endpoints)
-            {
-                var (changefreq, priority) = endpointMetadata[endpoint];
+                )
+            );
 
-                var url = new XElement(ns + "url",
-                    new XElement(ns + "loc", baseUrl + endpoint),
-                    new XElement(ns + "lastmod", today),
-                    new XElement(ns + "changefreq", changefreq),
-                    new XElement(ns + "priority", priority)
-                );
-
-                urlset.Add(url);
-            }
-
-            var sitemap = new XDocument(new XDeclaration("1.0", "UTF-8", "yes"), urlset);
-
-            return Content(sitemap.ToString(), "application/xml", Encoding.UTF8);
+            return Content(new XDocument(sitemap).ToString(), "application/xml", Encoding.UTF8);
         }
     }
+
 }
